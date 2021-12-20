89d20515c89ecebd6d2c9da912bf554ed5f0634c
Alex Jongman <alex.jongman@hu.nl>
2021-12-20 16:06:59 +0100

    spa using css display

--- src/view/drawer-menu/drawer-menu.js
+++ src/view/drawer-menu/drawer-menu.js
@@ -23,20 +23,35 @@ export class DrawerMenu extends LitElement {
       <mwc-drawer hasHeader type="modal" ?open=${this.menuOpen} @MDCTopAppBar:nav=${(e)=>this.openMenu(e)} @MDCDrawer:closed=${(e) =>this.closeMenu(e)}>
         <span slot="title">Menu</span>
         <div>
-          <p><a href="./about.html">About</a></p>
-          <p><a href="./index.html">Todo list</a></p>
+          <p @click=${this.showAbout}>About</p>
+          <p @click=${this.showTodo}>Todo list</p>
         </div>
         <div slot="appContent">
           <mwc-top-app-bar>
             <mwc-icon-button slot="navigationIcon" icon="menu"></mwc-icon-button>
             <div slot="title">My Todo App</div>
           </mwc-top-app-bar>
-          <slot></slot>
+            <div id="todo">
+              <add-task></add-task>
+              <task-list></task-list>
+            </div>
+            <div id="about" style="display: none">
+              <about-info></about-info>
+            </div>
         </div>
       </mwc-drawer>
     `;
   }
 
+  showAbout() {
+    this.shadowRoot.querySelector('#todo').style.display = "none";
+    this.shadowRoot.querySelector('#about').style.display = "block";
+  }
+
+  showTodo() {
+    this.shadowRoot.querySelector('#todo').style.display = "block";
+    this.shadowRoot.querySelector('#about').style.display = "none";
+  }
   openMenu(event) {
     this.menuOpen=true;
   }
