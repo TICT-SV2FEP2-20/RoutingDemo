fe791212a42272201942f0efd7ffb92be9efffb9
Alex Jongman <alex.jongman@hu.nl>
2023-01-09 10:15:52 +0100

    code dump

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
   input: 'src/index.js',
   output: {
     dir: 'dist',
-    format: 'cjs'
+    format: 'esm'
   },
   plugins: [nodeResolve()]
 };
--- src/service/AppService.js
+++ src/service/AppService.js
@@ -5,8 +5,22 @@ const rootReducer = combineReducers(
   {
     taskReducer: TaskReducers
   }
-)
+);
+
+const storageKey = 'todoApp';
+
+const saveState = (state) => localStorage.setItem(storageKey, JSON.stringify(state));
+const loadState = () => {
+  const json = localStorage.getItem(storageKey);
+  return json ? JSON.parse(json) : undefined;
+};
+
 export const store = createStore(
   rootReducer,
+  loadState(),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
